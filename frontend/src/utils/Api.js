23125dd64ef524ad8c8f1facd81ddffb0c4a831a
MarinaViriyalova <m.viriyalova@gmail.com>
2022-02-04 12:50:11 +0300

    auth bug

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -18,13 +18,16 @@ class Api {
         return Promise.reject(`Ошибка: ${res.status}`);
     }
 
-    getUserInfo() {
+    getUserInfo = () => {
         return fetch(`${this._baseUrl}/users/me`, {
-                method: 'GET',
-                headers: this._headers
-            })
-            .then(res => this._handleResponse(res))
-    }
+            method: "GET",
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+            },
+        }).then(res => this._handleResponse(res))
+    };
 
     editUserInfo(data) {
         return fetch(`${this._baseUrl}/users/me`, {
