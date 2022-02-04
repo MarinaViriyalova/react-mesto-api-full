c8c653e02fef56a71fa0951731bdff8dc2b2ceef
MarinaViriyalova <m.viriyalova@gmail.com>
2022-02-04 10:57:17 +0300

    cors check

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,12 +10,10 @@ module.exports = (req, res, next) => {
 
     const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-    console.log('1111', origin);
-
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Credentials', true);
-    }
+    // if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', 'https://viriyalova-mesto.nomoredomains.work');
+    res.header('Access-Control-Allow-Credentials', true);
+    // }
     if (method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
         res.header('Access-Control-Allow-Headers', requestHeaders);
